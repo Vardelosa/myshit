12e7df365c4d558e178fc671f6f440889322c2cf
Varde <vvvoha13@gmail.com>
2019-09-06 22:06:57 +0300

    да нихуя не вышло блять

--- LabaAlgoritms1/Program.cs
+++ LabaAlgoritms1/Program.cs
@@ -20,7 +20,22 @@ namespace LabaAlgoritms1
     {
         static void Main(string[] args)
         {
+            Person me = new Person("Vova", "Chakov", "Vladimirovich", "M", "dom16", 943255);
             Console.WriteLine("");
+            Person[] person1 = new Person[0];
+            Console.WriteLine(person1.);
+            AddPerson(person1, me);
+        }
+        Person[] AddPerson(ref Person[] persons, Person newPerson)
+        {
+            int newLength = persons.Length + 1;
+            Person[] result = new Person[newLength];
+            for (int i = 0; i < persons.Length; i++)
+            {
+                result[i] = persons[i];
+            }
+            result[newLength - 1] = newPerson;
+            return result;
         }
     }
   
@@ -28,25 +43,36 @@ namespace LabaAlgoritms1
     {
         public string name, surname, sursurname;       
     }
-  
+    struct Info
+    {
+        public string adres;
+        public int tnumber;
+    }
     struct Person
     {
         Personal pers;
-        bool sex;
-        public Person(string a, string b, string c, bool d)
+        string sex;
+        Info info;
+        public Person(string a, string b, string c, string d, string e, int g)
         {
             pers.name = a;
             pers.surname = b;
             pers.sursurname = c;
             sex = d;
+            info.adres = e;
+            info.tnumber = g;
         }
-        public void AddPerson(params Person[] persons)
-        {
-            for (int i = 0; i < persons.Length; i++)
-            {
-                
-            }
-        }
+        //public Person[] AddPerson(Person[] persons, Person newPerson)
+        //{
+        //    int newLength = persons.Length + 1;
+        //    Person[] result =new Person[newLength];
+        //    for(int i=0;i<persons.Length;i++)
+        //    {
+        //        result[i] = persons[i];
+        //    }
+        //    result[newLength - 1] = newPerson;
+        //    return result;
+        //}
         public void DeletePerson()
         {
 
@@ -54,6 +80,13 @@ namespace LabaAlgoritms1
         public void EditPerson()
         {
 
+        }
+        public string ToShortString()
+        {
+            string s = "Name: " + pers.name + "\r\n Surname: "
+                + pers.surname + "\r\n SurSurName: " + pers.sursurname +
+                "\r\n Sex: " + sex + "\r\n Adres: " + info.adres + "\r\n TNumber: " + info.tnumber;
+            return s;
         }
     }
 }
